10cb94f03c66c578a2d19c1247591b9676760e7c
tlau818 <tom_lau@yahoo.com>
2019-08-21 16:56:26 -0700

    commit version2 of binary tree code

---
+++ BinaryTree2/src/BinaryTree.java
@@ -0,0 +1,62 @@
+
+public class BinaryTree {
+	
+	private Node root;
+	
+	class Node{
+		private Node left;
+		private Node right;
+		
+		private int data;
+		private String name;
+		
+		public Node(int data, String str){
+			this.data = data;
+			this.name = str;
+		}
+		
+		public String toString(){
+			return "data: " + this.data + "name: " + this.name;
+		}
+	}
+	
+	public void addNode(int data, String name){
+		Node newNode = new Node(data, name);
+		if (root == null){
+			root = newNode;
+			return;
+		}else{
+			Node focusNode = root;
+			Node parent;
+			while(true){
+				parent = focusNode;
+				// left child
+				if (data < focusNode.data){
+					focusNode = focusNode.left;
+					if (focusNode == null){
+						parent.left = newNode;
+						return;
+					}
+				}else{
+					focusNode = focusNode.right;
+					if (focusNode == null){
+						parent.right = newNode;
+						return;
+					}
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	public void inOrderTraversalTree(Node focusNode){
+	
+		if (focusNode != null){
+			inOrderTraversalTree(focusNode.left);
+			System.out.println(focusNode);
+			inOrderTraversalTree(focusNode.right);
+		}
+	}
+}
